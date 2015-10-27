f8f193e07c1361021b28b9392fc0ce03517499be
Jun Yu <yujun@marvell.com>
2015-10-27 01:58:59 -0700

    Bluetooth HAL Module Source Code
    Linked to the old CL: https://partner-android-review.googlesource.com/#/c/480769/
    
    To add the condition: BLUETOOTH_DRIVER_HAL_PERIPHERAL to make sure the
    marvell bluetooth only gets to be built if the condition is set
    
    BUG=25486837
    
    Change-Id: Ib69134ab9e9bab88f4b67b7274ebd0a3febffe53

---
+++ peripheral/bt/Android.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(BLUETOOTH_DRIVER_HAL_PERIPHERAL), sd8xxx)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+BDROID_DIR := $(TOP_DIR)system/bt
+
+LOCAL_SRC_FILES := \
+	bt_vendor_mrvl.c
+
+LOCAL_C_INCLUDES += \
+	$(BDROID_DIR)/hci/include \
+	$(TOP_DIR)hardware/bsp/marvell/peripheral/libwireless
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libMarvellWireless
+
+LOCAL_MODULE := libbt-vendor
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_OWNER := marvell
+LOCAL_CFLAGS += -Werror
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
