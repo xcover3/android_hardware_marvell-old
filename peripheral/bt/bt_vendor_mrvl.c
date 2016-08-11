de88369898a5df64d95a8e169f4c6766d676f412
Alex Deymo <deymo@google.com>
2016-08-11 10:52:50 -0700

    Define what's TRUE and what's FALSE in life.
    Bug: None
    TEST=`make`
    
    Change-Id: I20a81a9943f4c3f09fc9e312e19013c02a631e70

--- peripheral/bt/bt_vendor_mrvl.c
+++ peripheral/bt/bt_vendor_mrvl.c
@@ -37,6 +37,13 @@
 #include "marvell_wireless.h"
 #include "bt_hci_bdroid.h"
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+
 /* define log functions here */
 #define LOG_TAG "libbt-vendor-mrvl"
 #include <cutils/log.h>
