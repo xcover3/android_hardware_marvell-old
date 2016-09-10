65daf982aeb01251e3eacf18ec1c1ea80e92028f
CTXz <ctx.xda@gmail.com>
2016-09-10 12:11:02 +0200

    libGAL: Replace with source available alternative
    The proprietary libGAL has a hard time initializing the EGL compontents correcty, unfortunately it is hard to debug proprietary components
    
    After a bit of researched I have found, what seems to be a source release of older vivante GPU libraries. After re-locating and mixing source files of different
    GPUs a compilable code has been achieved.
    
    I do not intend to sell any of vivante's works. If you own rights for the vivante code that I used during this project and wish to have it removed, please contact me via
    ctx.xda@gmail.com and all claimed vivante content will be removed.
    
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- graphics/libGAL/Android.mk
+++ graphics/libGAL/Android.mk
@@ -15,26 +15,99 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/Android.mk.def
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := \
-	libGAL.a \
-	libhalarchuser.a \
-	libhalosuser.a
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
 
+LOCAL_SRC_FILES := \
+	gc_hal_user_hardware_blt.c \
+	gc_hal_user_hardware_clear.c \
+	gc_hal_user_hardware_filter_blt_de.c \
+	gc_hal_user_hardware_filter_blt_vr.c \
+	gc_hal_user_hardware.c \
+	gc_hal_user_hardware_pattern.c \
+	gc_hal_user_hardware_pipe.c \
+	gc_hal_user_hardware_primitive.c \
+	gc_hal_user_hardware_query.c \
+	gc_hal_user_hardware_source.c \
+	gc_hal_user_hardware_target.c \
+	gc_hal_user_brush.c \
+	gc_hal_user_brush_cache.c \
+	gc_hal_user_buffer.c \
+	gc_hal_user.c \
+	gc_hal_user_dump.c \
+	gc_hal_user_heap.c \
+	gc_hal_user_paint.c \
+	gc_hal_user_path.c \
+	gc_hal_user_profiler.c \
+	gc_hal_user_query.c \
+	gc_hal_user_queue.c \
+	gc_hal_user_raster.c \
+	gc_hal_user_rect.c \
+	gc_hal_user_surface.c \
+	gc_hal_user_vg.c \
+	gc_hal_user_debug.c \
+	gc_hal_user_os.c \
+	gc_hal_user_math.c
 
-include $(CLEAR_VARS)
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-	libGAL \
-	libhalarchuser \
-	libhalosuser
+ifneq ($(VIVANTE_NO_3D),1)
+LOCAL_SRC_FILES += \
+	gc_hal_user_hardware_engine.c \
+	gc_hal_user_hardware_frag_proc.c \
+	gc_hal_user_hardware_texture.c \
+	gc_hal_user_hardware_stream.c \
+	gc_hal_user_hardware_composition.c \
+	gc_hal_user_engine.c \
+	gc_hal_user_index.c \
+	gc_hal_user_md5.c \
+	gc_hal_user_mem.c \
+	gc_hal_user_texture.c \
+	gc_hal_user_vertex.c \
+	gc_hal_user_vertex_array.c
 
-LOCAL_SHARED_LIBRARIES := libcutils libutils libdl liblog libgputex
+LOCAL_SRC_FILES += \
+	gc_hal_user_hardware_code_gen.c \
+	gc_hal_user_hardware_linker.c \
+	gc_hal_user_hardware_shader.c \
+	gc_hal_user_compiler.c \
+	gc_hal_user_linker.c \
+	gc_hal_optimizer.c \
+	gc_hal_optimizer_dump.c \
+	gc_hal_optimizer_util.c \
+	gc_hal_user_loadtime_optimizer.c
+endif
 
-LOCAL_LDLIBS := -ldl -llog
-LOCAL_MODULE := libGAL
-LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := \
+	$(CFLAGS)
+
+ifeq ($(VIVANTE_NO_3D),1)
+LOCAL_CFLAGS += \
+	-DVIVANTE_NO_3D
+endif
+
+LOCAL_C_INCLUDES := \
+	hardware/marvell/pxa1088/graphics/libGAL/include \
+
+ifeq ($(USE_LINUX_MODE_FOR_ANDROID), 1)
+	LOCAL_C_INCLUDES += \
+		hardware/marvell/pxa1088/graphics/include/sdk/inc
+
+	LOCAL_SRC_FILES += \
+		gc_hal_user_fbdev.c
+endif
+
+LOCAL_LDFLAGS := \
+	-Wl,-z,defs \
+	-Wl,--version-script=$(LOCAL_PATH)/libGAL.map
+
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libdl
+
+LOCAL_GENERATED_SOURCES := \
+
+LOCAL_MODULE         := libGAL
+LOCAL_MODULE_TAGS    := optional
 LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
+
