e50177bc66205b736a6c516737ea32f5532f4f63
Matt07211 <matthew.hanford2@gmail.com>
2017-04-14 12:45:18 +0000

    Merge top Level Android.mk files

--- Android.mk
+++ Android.mk
@@ -1,3 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_BOARD_PLATFORM), mrvl)
+ifeq ($(TARGET_SOC), pxa1088)
+
+pxa1088_dirs := \
+	MarvellWirelessDaemon \
+	gpu-engine \
+	graphics \
+	hwcomposer \
+	hwcomposerGC \
+	ipplib \
+	libMarvellWireless \
+	libstagefrighthw \
+	marvell-gralloc \
+	phycontmem-lib \
+	sd8787 \
+	vmeta-lib
+
+include $(call all-named-subdir-makefiles,$(pxa1088_dirs))
+
+endif
+endif
+
 ifneq ($(BOARD_HAVE_BLUETOOTH_MRVL),)
-include $(call all-named-subdir-makefiles,libbt-vendor)
+	include $(call all-named-subdir-makefiles,libbt-vendor)
 endif
